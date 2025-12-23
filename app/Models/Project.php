fb21839dbaf25402951d4f4bf561ff670869302a
Adem-elwafi <adem.elwafi.dev@gmail.com>
2025-12-23 13:35:27 +0100

    feat(models): add Project and Task models with relationships

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,9 +2,24 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['name', 'description', 'user_id'];
+
+    // A project belongs to a user (owner)
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    // A project has many tasks
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
---
+++ app/Models/Task.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title', 'description', 'status', 'priority', 'due_date', 'project_id', 'user_id'
+    ];
+
+    // A task belongs to a project
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    // A task may be assigned to a user
+    public function assignee()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
